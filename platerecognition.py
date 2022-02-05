c68833312a8f2f4cac836fe75eaa297c033c18dd
igorgrcc <903630@edu.p.lodz.pl>
2022-02-05 19:28:54 +0100

    first commit

---
+++ platerecognition.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+#import numpy as np
+#import pandas as pd
+#import seaborn as sns
+#from matplotlib import pyplot as plt
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
