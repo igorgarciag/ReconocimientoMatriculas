bdb37555ac310293e68e044680b18b2e7c56c186
igorgrcc <903630@edu.p.lodz.pl>
2022-02-05 20:44:32 +0100

    imports added

--- platerecognition.py
+++ platerecognition.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import cv2
+import os
+import glob
 
-#import numpy as np
-#import pandas as pd
-#import seaborn as sns
-#from matplotlib import pyplot as plt
-
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
